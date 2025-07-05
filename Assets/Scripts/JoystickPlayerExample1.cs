170f3910cab6ab0d724f5603d80d74ba5f5a2964
Fatih <fatih.emre.gd@gmail.com>
2025-07-05 20:53:45 +0300

    Camera-and-Controller

---
+++ Assets/Scripts/JoystickPlayerExample1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed = 5f;
+    public VariableJoystick variableJoystick;
+
+    public void Update()
+    {
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            // Doğrudan hareket
+            transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+            // Karakter yönünü joystick yönüne döndür
+            Quaternion toRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, Time.deltaTime * 10f);
+        }
+    }
+}
