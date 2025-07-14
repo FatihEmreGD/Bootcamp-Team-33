240295b53369ef30f0ac6162cd72455fe74d6c60
senayengin <asenayengin@gmail.com>
2025-07-14 20:07:58 +0300

    tarif hata düzeltildi

--- Assets/Scripts/CookingSystem/Ingredient.cs
+++ Assets/Scripts/CookingSystem/Ingredient.cs
@@ -1,33 +1,10 @@
-using UnityEditor;
 using UnityEngine;
 
-[InitializeOnLoad]
-public static class FolderIconSetter
+[CreateAssetMenu(fileName = "New Ingredient", menuName = "Cooking/Ingredient")]
+public class Ingredient : ScriptableObject
 {
-    static FolderIconSetter()
-    {
-        EditorApplication.projectWindowItemOnGUI += OnProjectWindowItemGUI;
-    }
-
-    static void OnProjectWindowItemGUI(string guid, Rect rect)
-    {
-        string path = AssetDatabase.GUIDToAssetPath(guid);
-
-        // Sadece klasörler için çalýþsýn
-        if (AssetDatabase.IsValidFolder(path))
-        {
-            // Klasör yolu buraya eþitse simgeyi göster
-            if (path == "Assets/CookingSystem/ScriptableObjects/Ingredients")
-            {
-                // PNG simge yolu (örnek olarak 'carrot.png' kullanýldý)
-                Texture icon = AssetDatabase.LoadAssetAtPath<Texture>("Assets/ingredient_png/carrot.png");
-
-                if (icon != null)
-                {
-                    GUI.DrawTexture(new Rect(rect.x, rect.y, 16, 16), icon);
-                }
-            }
-        }
-    }
+    public string ingredientName;
+    public Sprite icon;
+    public float cookTime;
+    public float burnTime;
 }
-
